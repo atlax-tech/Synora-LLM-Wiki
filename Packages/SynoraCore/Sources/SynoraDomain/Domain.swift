c984547247bb5fd6153370c0528ac4db341cdc76
AtlaxTech <atlax-tech@outlook.com>
2026-09-07 14:04:08 +0800

    feat(p2): define block document schema and invariants

--- Packages/SynoraCore/Sources/SynoraDomain/Domain.swift
+++ Packages/SynoraCore/Sources/SynoraDomain/Domain.swift
@@ -52,31 +52,342 @@ public struct Library: Codable, Hashable, Sendable {
   }
 }
 
+public enum RecordKind: String, Codable, CaseIterable, Hashable, Sendable {
+  case note
+  case journal
+}
+
+public enum BlockType: Codable, Hashable, Sendable {
+  case paragraph
+  case heading1
+  case heading2
+  case heading3
+  case bulletedList
+  case numberedList
+  case task
+  case quote
+  case code
+  case divider
+  case table
+  case toggle
+  case callout
+  case image
+  case gallery
+  case video
+  case audio
+  case pdf
+  case file
+  case link
+  case unknown(String)
+
+  private var wireValue: String {
+    switch self {
+    case .paragraph: "paragraph"
+    case .heading1: "heading1"
+    case .heading2: "heading2"
+    case .heading3: "heading3"
+    case .bulletedList: "bulleted-list"
+    case .numberedList: "numbered-list"
+    case .task: "task"
+    case .quote: "quote"
+    case .code: "code"
+    case .divider: "divider"
+    case .table: "table"
+    case .toggle: "toggle"
+    case .callout: "callout"
+    case .image: "image"
+    case .gallery: "gallery"
+    case .video: "video"
+    case .audio: "audio"
+    case .pdf: "pdf"
+    case .file: "file"
+    case .link: "link"
+    case .unknown(let value): value
+    }
+  }
+
+  public init(from decoder: Decoder) throws {
+    let value = try decoder.singleValueContainer().decode(String.self)
+    switch value {
+    case "paragraph": self = .paragraph
+    case "heading1": self = .heading1
+    case "heading2": self = .heading2
+    case "heading3": self = .heading3
+    case "bulleted-list": self = .bulletedList
+    case "numbered-list": self = .numberedList
+    case "task": self = .task
+    case "quote": self = .quote
+    case "code": self = .code
+    case "divider": self = .divider
+    case "table": self = .table
+    case "toggle": self = .toggle
+    case "callout": self = .callout
+    case "image": self = .image
+    case "gallery": self = .gallery
+    case "video": self = .video
+    case "audio": self = .audio
+    case "pdf": self = .pdf
+    case "file": self = .file
+    case "link": self = .link
+    default: self = .unknown(value)
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(wireValue)
+  }
+
+  public var acceptsChildren: Bool {
+    switch self {
+    case .bulletedList, .numberedList, .quote, .table, .toggle, .callout, .gallery:
+      true
+    default:
+      false
+    }
+  }
+}
+
+public enum JSONValue: Codable, Hashable, Sendable {
+  case object([String: JSONValue])
+  case array([JSONValue])
+  case string(String)
+  case number(Double)
+  case bool(Bool)
+  case null
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if container.decodeNil() { self = .null }
+    else if let value = try? container.decode(Bool.self) { self = .bool(value) }
+    else if let value = try? container.decode(Double.self) { self = .number(value) }
+    else if let value = try? container.decode(String.self) { self = .string(value) }
+    else if let value = try? container.decode([JSONValue].self) { self = .array(value) }
+    else { self = .object(try container.decode([String: JSONValue].self)) }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    switch self {
+    case .object(let value): try value.encode(to: encoder)
+    case .array(let value): try value.encode(to: encoder)
+    case .string(let value): try value.encode(to: encoder)
+    case .number(let value): try value.encode(to: encoder)
+    case .bool(let value): try value.encode(to: encoder)
+    case .null: var container = encoder.singleValueContainer(); try container.encodeNil()
+    }
+  }
+}
+
 public struct Record: Codable, Hashable, Sendable {
   public let id: UUID
   public var title: String
   public var revision: Int
+  public var kind: RecordKind
+  public var journalDate: Date?
+  public var metadata: [String: String]
+  public var unknownFields: [String: JSONValue]
 
-  public init(id: UUID, title: String, revision: Int = 0) {
+  public init(
+    id: UUID,
+    title: String,
+    revision: Int = 0,
+    kind: RecordKind = .note,
+    journalDate: Date? = nil,
+    metadata: [String: String] = [:],
+    unknownFields: [String: JSONValue] = [:]
+  ) {
     self.id = id
     self.title = title
     self.revision = revision
+    self.kind = kind
+    self.journalDate = journalDate
+    self.metadata = metadata
+    self.unknownFields = unknownFields
+  }
+
+  public init(id: UUID, title: String, revision: Int) {
+    self.init(id: id, title: title, revision: revision, kind: .note)
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case id, title, revision, kind, journalDate, metadata, unknownFields
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.init(
+      id: try container.decode(UUID.self, forKey: .id),
+      title: try container.decode(String.self, forKey: .title),
+      revision: try container.decodeIfPresent(Int.self, forKey: .revision) ?? 0,
+      kind: try container.decodeIfPresent(RecordKind.self, forKey: .kind) ?? .note,
+      journalDate: try container.decodeIfPresent(Date.self, forKey: .journalDate),
+      metadata: try container.decodeIfPresent([String: String].self, forKey: .metadata) ?? [:],
+      unknownFields: try container.decodeIfPresent([String: JSONValue].self, forKey: .unknownFields) ?? [:]
+    )
   }
 }
 
 public struct Block: Codable, Hashable, Sendable {
   public let id: UUID
   public let recordID: UUID
+  public var parentID: UUID?
+  public var type: BlockType
+  public var orderKey: Int64
   public var position: Int
   public var text: String
+  public var attributes: [String: String]
+  public var unknownFields: [String: JSONValue]
   public var revision: Int
 
-  public init(id: UUID, recordID: UUID, position: Int, text: String, revision: Int = 0) {
+  public init(
+    id: UUID,
+    recordID: UUID,
+    position: Int,
+    text: String,
+    revision: Int = 0,
+    parentID: UUID? = nil,
+    type: BlockType = .paragraph,
+    orderKey: Int64? = nil,
+    attributes: [String: String] = [:],
+    unknownFields: [String: JSONValue] = [:]
+  ) {
     self.id = id
     self.recordID = recordID
+    self.parentID = parentID
+    self.type = type
+    self.orderKey = orderKey ?? Int64(position) * 1024
     self.position = position
     self.text = text
+    self.attributes = attributes
+    self.unknownFields = unknownFields
     self.revision = revision
+  }
+
+  public init(id: UUID, recordID: UUID, position: Int, text: String, revision: Int) {
+    self.init(
+      id: id, recordID: recordID, position: position, text: text, revision: revision,
+      parentID: nil, type: .paragraph, orderKey: Int64(position) * 1024)
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case id, recordID, parentID, type, orderKey, position, text, attributes, unknownFields, revision
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let position = try container.decodeIfPresent(Int.self, forKey: .position) ?? 0
+    self.init(
+      id: try container.decode(UUID.self, forKey: .id),
+      recordID: try container.decode(UUID.self, forKey: .recordID),
+      position: position,
+      text: try container.decodeIfPresent(String.self, forKey: .text) ?? "",
+      revision: try container.decodeIfPresent(Int.self, forKey: .revision) ?? 0,
+      parentID: try container.decodeIfPresent(UUID.self, forKey: .parentID),
+      type: try container.decodeIfPresent(BlockType.self, forKey: .type) ?? .paragraph,
+      orderKey: try container.decodeIfPresent(Int64.self, forKey: .orderKey) ?? Int64(position) * 1024,
+      attributes: try container.decodeIfPresent([String: String].self, forKey: .attributes) ?? [:],
+      unknownFields: try container.decodeIfPresent([String: JSONValue].self, forKey: .unknownFields) ?? [:]
+    )
+  }
+}
+
+public enum BlockTreeError: Error, Equatable, Sendable {
+  case duplicateID(UUID)
+  case missingParent(UUID)
+  case crossRecordParent(UUID)
+  case cycle(UUID)
+  case invalidChild(UUID)
+}
+
+public struct BlockDocument: Codable, Hashable, Sendable {
+  public let recordID: UUID
+  public private(set) var blocks: [Block]
+
+  public init(recordID: UUID, blocks: [Block] = []) throws {
+    self.recordID = recordID
+    self.blocks = blocks
+    try validate()
+  }
+
+  public func validate() throws {
+    var IDs = Set<UUID>()
+    var blockByID: [UUID: Block] = [:]
+    for block in blocks {
+      guard IDs.insert(block.id).inserted else { throw BlockTreeError.duplicateID(block.id) }
+      blockByID[block.id] = block
+      guard block.recordID == recordID else { throw BlockTreeError.crossRecordParent(block.id) }
+      guard block.parentID != block.id else { throw BlockTreeError.cycle(block.id) }
+      if let parentID = block.parentID {
+        guard let parent = blockByID[parentID] else { throw BlockTreeError.missingParent(parentID) }
+        guard parent.recordID == recordID else { throw BlockTreeError.crossRecordParent(block.id) }
+        guard parent.type.acceptsChildren else { throw BlockTreeError.invalidChild(block.id) }
+        var ancestor = parentID
+        var visited = Set<UUID>()
+        while let current = blockByID[ancestor] {
+          guard visited.insert(current.id).inserted else { throw BlockTreeError.cycle(block.id) }
+          if current.parentID == block.id { throw BlockTreeError.cycle(block.id) }
+          guard let next = current.parentID else { break }
+          ancestor = next
+        }
+      }
+    }
+  }
+
+  public func children(of parentID: UUID? = nil) -> [Block] {
+    blocks.filter { $0.parentID == parentID }.sorted {
+      ($0.orderKey, $0.id.uuidString) < ($1.orderKey, $1.id.uuidString)
+    }
+  }
+
+  public func block(id: UUID) -> Block? { blocks.first { $0.id == id } }
+
+  public func inserting(_ block: Block, before siblingID: UUID? = nil) throws -> Self {
+    var copy = self
+    var inserted = block
+    let siblings = children(of: block.parentID)
+    let index = siblingID.flatMap { sibling in siblings.firstIndex { $0.id == sibling } } ?? siblings.count
+    let previous = index > 0 ? siblings[index - 1].orderKey : nil
+    let next = index < siblings.count ? siblings[index].orderKey : nil
+    inserted.orderKey = Self.orderKey(previous: previous, next: next)
+    inserted.position = index
+    copy.blocks.append(inserted)
+    try copy.validate()
+    return copy
+  }
+
+  public func moving(id: UUID, to parentID: UUID?, before siblingID: UUID? = nil) throws -> Self {
+    guard let source = block(id: id) else { throw BlockTreeError.missingParent(id) }
+    var copy = self
+    copy.blocks.removeAll { $0.id == id }
+    var moved = source
+    moved.parentID = parentID
+    return try copy.inserting(moved, before: siblingID)
+  }
+
+  public func deleting(id: UUID) throws -> Self {
+    guard block(id: id) != nil else { throw BlockTreeError.missingParent(id) }
+    let removed = Set([id] + descendants(of: id).map(\.id))
+    return try Self(recordID: recordID, blocks: blocks.filter { !removed.contains($0.id) })
+  }
+
+  public func descendants(of id: UUID) -> [Block] {
+    var result: [Block] = []
+    var pending = [id]
+    while let parent = pending.popLast() {
+      let children = blocks.filter { $0.parentID == parent }
+      result.append(contentsOf: children)
+      pending.append(contentsOf: children.map(\.id))
+    }
+    return result
+  }
+
+  private static func orderKey(previous: Int64?, next: Int64?) -> Int64 {
+    switch (previous, next) {
+    case let (left?, right?) where right - left > 1: return left + (right - left) / 2
+    case let (left?, nil): return left + 1024
+    case let (nil, right?): return right - 1024
+    default: return 0
+    }
   }
 }
 
--- Packages/SynoraCore/Tests/SynoraDomainTests/DomainTests.swift
+++ Packages/SynoraCore/Tests/SynoraDomainTests/DomainTests.swift
@@ -64,3 +64,53 @@ func operationHashUsesCanonicalBytes() {
   )
   #expect(left.hash == "3f23cfd87d3fb1d6333e02fde4f1b7eb8ef9b18997d0b05b6179128ec899ef86")
 }
+
+@Test
+func blockDocumentPreservesIdentityOrderingAndUnknownFields() throws {
+  let recordID = UUID(uuidString: "00000000-0000-4000-8000-000000000010")!
+  let firstID = UUID(uuidString: "00000000-0000-4000-8000-000000000011")!
+  let secondID = UUID(uuidString: "00000000-0000-4000-8000-000000000012")!
+  let first = Block(
+    id: firstID, recordID: recordID, position: 0, text: "中文🙂",
+    type: .bulletedList, unknownFields: ["future": .object(["enabled": .bool(true)])])
+  let second = Block(
+    id: secondID, recordID: recordID, position: 1, text: "子项", parentID: firstID,
+    type: .paragraph)
+  let document = try BlockDocument(recordID: recordID, blocks: [first, second])
+
+  #expect(document.children().map(\.id) == [firstID])
+  #expect(document.descendants(of: firstID).map(\.id) == [secondID])
+  let encoded = try JSONEncoder().encode(document)
+  let decoded = try JSONDecoder().decode(BlockDocument.self, from: encoded)
+  #expect(decoded == document)
+}
+
+@Test
+func blockDocumentRejectsInvalidParentsAndCycles() throws {
+  let recordID = UUID()
+  let parentID = UUID()
+  let childID = UUID()
+  #expect(throws: BlockTreeError.invalidChild(childID)) {
+    try BlockDocument(
+      recordID: recordID,
+      blocks: [
+        Block(id: parentID, recordID: recordID, position: 0, text: "plain"),
+        Block(id: childID, recordID: recordID, position: 1, text: "child", parentID: parentID),
+      ])
+  }
+  #expect(throws: BlockTreeError.missingParent(parentID)) {
+    try BlockDocument(
+      recordID: recordID,
+      blocks: [Block(id: childID, recordID: recordID, position: 0, text: "child", parentID: parentID)])
+  }
+}
+
+@Test
+func legacyBlockPayloadDecodesWithParagraphDefaults() throws {
+  let id = UUID(uuidString: "00000000-0000-4000-8000-000000000020")!
+  let recordID = UUID(uuidString: "00000000-0000-4000-8000-000000000021")!
+  let data = Data("{\"id\":\"\(id.uuidString)\",\"recordID\":\"\(recordID.uuidString)\",\"position\":2,\"text\":\"legacy\",\"revision\":3}".utf8)
+  let block = try JSONDecoder().decode(Block.self, from: data)
+  #expect(block.type == .paragraph)
+  #expect(block.orderKey == 2048)
+}
